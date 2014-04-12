82749fac419b20ae5922e1889eed2210d4183eb5
Alex Wright <aawqkc@mail.mst.edu>
2014-04-12 14:12:50 -0500

    more bfs stuff

---
+++ Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Point
+{
+    public int X;
+    public int Y;
+
+    public Point() : this(0, 0)
+    {
+    }
+
+    public Point(int X, int Y)
+    {
+        this.X = X;
+        this.Y = Y;
+    }
+}
---
+++ Searcher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Node
+{
+    public Node(Node parent, Point point)
+    {
+        this.parent = parent;
+        this.point = point;
+    }
+
+    public Node parent;
+    public Point point;
+}
+
+class Searcher
+{
+    public Searcher()
+    {
+    }
+
+    public IEnumerable<Point> findPath(Droid start, Bb target)
+    {
+        Queue<Node> frontier = new Queue<Node>();
+        HashSet<Point> explored = new HashSet<Point>();
+        frontier.Enqueue(new Node(null, new Point(start.X, start.Y)));
+        explored.Add(new Point(start.X, start.Y));
+        while (frontier.Count > 0)
+        {
+            Node current = frontier.Dequeue();
+
+            if (target.getValueFromSpot(current.point.X, current.point.Y))
+            {
+                IList<Point> path = new List<Point>();
+                while (current.parent != null)
+                {
+                    path.Insert(0, current.point);
+                    current = current.parent;
+                }
+                return path;
+            }
+
+            for (int i = -1; i < 2; i += 2)
+            {
+                Point pX = new Point(current.point.X + i, current.point.Y);
+                Point pY = new Point(current.point.X, current.point.Y + i);
+                if (!explored.Contains(pX))
+                {
+                    explored.Add(pX);
+                    frontier.Enqueue(pX);
+                }
+                if (!explored.Contains(pY))
+                {
+                    explored.Add(pY);
+                    frontier.Enqueue(pY);
+                }
+            }
+        }
+
+        return new List<Point>();  // No valid path was found
+    }
+}
