cf09e613e5d519f157148345090637b9eee57b7b
Austin Gantner <austingantner@gmail.com>
2014-04-12 16:09:31 -0500

    added goto in mission.cs

--- Mission.cs
+++ Mission.cs
@@ -11,12 +11,14 @@ class Mission
     public MissionTypes missionType;
     public Droid agent;
     public Func<Point, bool> target;
+    public Func<Point, bool> isWalkable;
 
-    public Mission(MissionTypes missionType, Droid agent, Func<Point, bool> target)
+    public Mission(MissionTypes missionType, Droid agent, Func<Point, bool> target, Func<Point, bool> isWalkable)
     {
         this.missionType = missionType;
         this.agent = agent;
         this.target = target;
+        this.isWalkable = isWalkable;
     }
 }
 
